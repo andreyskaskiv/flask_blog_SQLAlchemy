efe10554b152ce66ef8083460b58fe78c6669cdd
andrey <andreyskaskiv.py@gmail.com>
2023-02-11 15:43:28 +0200

    3. Database create, SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_wtf.csrf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
 
 from app.config import config
 from app.error_handlers import internal_server_error, page_not_found
 
+db = SQLAlchemy()
 
 def create_app(config_name='default'):
     app = Flask(__name__)
@@ -19,6 +21,8 @@ def create_app(config_name='default'):
     csrf = CSRFProtect(app)
     csrf.init_app(app)
     app.config['CSRF'] = csrf
+
+    db.init_app(app)
 
     from app.main import main
     from app.auth import auth
